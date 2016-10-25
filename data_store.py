67708257023061876fb57667af151113f84b4fb0
jerome moreau <jejemoreau@macbook-pro-de-jerome.home>
2016-10-25 20:48:30 +0200

    Added documentation

--- data_store.py
+++ data_store.py
@@ -1,19 +1,36 @@
 import pymongo
 
+
 class MongoDataStore(object):
     def __init__(self, db_name='TICKS', db_adress='localhost'):
+        """
+        Save the ticks data to MongoDB
+
+        :param db_name: name of the database in MongoDB
+        :param db_adress: adress of your mongoDB instance
+        """
         self.database = self._connect_to_mongodb(db_name, db_adress)
 
 
     def _connect_to_mongodb(self, db_name, db_adress):
+        """
+
+        :param db_name: name of the database in MongoDB
+        :param db_adress: adress of your mongoDB instance
+        :return: database's connection
+        """
         client = pymongo.MongoClient(db_adress)
         database = client[db_name]
         return database
 
 
     def recordTick(self,tick):
+        """
+        Insert the tick data into th database
+        :param tick: dict containing at least: (time,pair,bid,ask) data
+        """
         library = self.database['oanda']
-        tick = {"time":tick['time'],"pair":tick["instrument"],"bid":tick['bid'],"ask":tick['ask']}
+        tick = {"time": tick['time'], "pair": tick["instrument"], "bid": tick['bid'], "ask": tick['ask']}
         print(tick)
 
         library.insert_one(tick)
