1aa23303a29154d2abe09cadbfd24e50c5db90ed
Jerome <jerome.moreau2@gmail.com>
2016-11-18 19:44:04 +0100

    Added settings.json for configuration.

--- tests/test_mongoDataStore.py
+++ tests/test_mongoDataStore.py
@@ -2,9 +2,34 @@ from unittest import TestCase
 from data_store import MongoDataStore
 
 class TestMongoDataStore(TestCase):
+
+    def setUp(self):
+        """
+        run when starting the tests
+        Initialize the data_store and a collection.
+        """
+        self.mongo = MongoDataStore()
+        self.library = self.mongo.database['testCollection']
+
+
     def test__connect_to_mongodb(self):
-       # mongo = MongoDataStore().database.co
-        self.fail()
+        self.assertEqual(self.library.name, 'testCollection', msg="Failed to create a document collection")
 
     def test_recordTick(self):
-        self.fail()
+        result = self.library.insert_one({'testDocument': 1})
+        self.assertIsNotNone(result.inserted_id, msg="Failed to insert in the database")
+
+        data = {}
+        result2 = self.library.insert_one(data)
+
+
+
+
+
+    def tearDown(self):
+        """
+        run after the tests
+        remove the document collection in the database
+        """
+        self.mongo.database.drop_collection('test')
+        super().tearDown()
